babb503b165b851b93f4b76479a19447d63a4c29
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-04-02 04:49:35 +0000

    testing with new region

--- terraform/prod.tfvars
+++ terraform/prod.tfvars
@@ -2,7 +2,7 @@
 vpc_cidr = "10.0.0.0/16"
 public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
 private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
-availability_zones = ["us-east-1a", "us-east-1b"]
+availability_zones = ["eu-west-3a", "eu-west-3b"]
 
 sg_name = "web-sg"
 sg_description = "Security group for web servers"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,12 @@ terraform {
   backend "s3" {
     bucket         = "uc01-bucket"
     key            = "terraform/statefile.tfstate"
-    region         = "eu-west-2"
+    region         = "eu-west-"3
   #  dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-west-3"
 }
